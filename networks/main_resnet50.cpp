4958acac2b3d3c3c2453a58d2dfff29e6445feeb
luborozporka <lubomir.rozporka@gmail.com>
2026-05-18 00:16:54 +0200

    Give each standalone client its own silent-OT cache file

--- networks/main_resnet50.cpp
+++ networks/main_resnet50.cpp
@@ -1,3 +1,4 @@
+#include "globals.h"
 #include "networks/networks.h"
 #include "utils/ArgMapping/ArgMapping.h"
 
@@ -22,6 +23,10 @@ int main(int argc, char **argv) {
   amap.arg("k", kScale, "bits of scale");
 
   amap.parse(argc, argv);
+
+  // Tag the pre-OT cache files by port so concurrent client processes
+  // do not mess with each other's caches via the shared `./data/pre_ot_data_reg_*_bob` path
+  g_session_tag = "p" + std::to_string(port);
 
   run_resnet50_inference(party, port, address, num_threads, bitlength, kScale);
 }
